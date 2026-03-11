949e3e4520f771de2f07c8d5ed9d2018936ce5f2
MARIA <MARIA@LAPTOP-T0VQDG70>
2026-03-11 17:50:17 +0100

    Esta es la rama main

--- src/CompararCon50.java
+++ src/CompararCon50.java
@@ -14,14 +14,14 @@ public class CompararCon50 {
 
         String numeroEntrada;
         if (args.length == 0) {
-            numeroEntrada = "15";
+            numeroEntrada = "13";
         } else {
             numeroEntrada = args[0];
         }
         if (args.length != 1) {
             System.out.println("Es necesario pasar un argumento entero");
             System.out.println("Sin argumentos se ejecuta con un valor fijo " + numeroEntrada);
-            System.out.println("asdfasdf");
+            System.out.println("asdfasdfadsfasgasdg");
         }
 
         try {
