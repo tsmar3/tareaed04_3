196e2f0ed4fd88a23bfb1b37892b65e75c9dda0d
MARIA <MARIA@LAPTOP-T0VQDG70>
2026-03-11 17:32:58 +0100

    Versión inicial sin modificar

---
+++ src/CompararCon50.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Borja González García
+ */
+public class CompararCon50 {
+
+    public static void main(String[] args) {
+
+        String numeroEntrada;
+        if (args.length == 0) {
+            numeroEntrada = "13";
+        } else {
+            numeroEntrada = args[0];
+        }
+        if (args.length != 1) {
+            System.out.println("Es necesario pasar un argumento entero");
+            System.out.println("Sin argumentos se ejecuta con un valor fijo " + numeroEntrada);
+        }
+
+        try {
+            int numero = Integer.parseInt(numeroEntrada);
+
+            if (numero > 50) {
+                System.out.println("El numero es mayor que 50.");
+            } else if (numero < 50) {
+                System.out.println("El numero es menor que 50.");
+            } else {
+                System.out.println("El numero es igual a 50.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error: el argumento debe ser un número entero.");
+        }
+    }
+}
